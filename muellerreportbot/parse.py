a504ca6ea44ab6d1af2ec42e6b2933757036bee2
Jeffrey D. McGovern <jeffreydmcgovern@gmail.com>
2019-04-19 02:07:20 -0700

    done, for now

--- muellerreportbot/parse.py
+++ muellerreportbot/parse.py
@@ -6,6 +6,11 @@ import errno
 
 import argparse
 
+import re
+from pprint import pprint
+
+import json
+
 # Logging
 import logging
 from logging import handlers
@@ -19,20 +24,61 @@ LOGGER.addHandler(SH)
 LOGGER.addHandler(FH)
 
 DEFAULT_FILENAME="MuellerReport.txt"
+DEFAULT_OUTPUT="cbdq.json"
 DESCRIPTION = """Parses the MuellerReport file into the appropriate data structures to enable text generation."""
 def get_arg_parser():
     parser = argparse.ArgumentParser(prog=sys.argv[0], description=DESCRIPTION)
     parser.add_argument("-f", "--filename",
             help = "file to parse (default is %s)" % DEFAULT_FILENAME)
+    parser.add_argument("-o", "--output",
+            help = "file to save the Cheap Bots Done Quick JSON to (default is %s)" % DEFAULT_OUTPUT)
     parser.add_argument("-i", "--info",
             help = "set console logging output to INFO")
     parser.add_argument("-d", "--debug",
             help = "set console logging output to DEBUG")
     parser.set_defaults(
-            filename = DEFAULT_FILENAME
+            filename = DEFAULT_FILENAME,
+            output = DEFAULT_OUTPUT
             )
     return parser
 
+def ngrams(doc, n=2):
+    return ngrams
+
+def get_sents(doc):
+
+    # Get the Harm To Whatever stuff as their own sentences
+    capture_inbetween = re.sub(r"(?<!\.)\n{2,}", "` ", doc)
+
+    # Remove extra whitespace within
+    doc_text = ' '.join(capture_inbetween.split())
+
+    # Create a list of sentences
+    sent_split = re.split("((?<!(v|[A-Z]|[0-9]))(?<!(Jan|Feb|Mar|Apr|Jun|Jul|Aug|Oct|Nov|Dec))[.?!~`])", doc_text)
+
+    # Filter out the empty garbage items
+    non_empty = [s.strip() for s in sent_split if s];
+
+    # Append the punctuation to the previous sentence
+    punct_set = set(".?!")
+    sents = []
+    if not non_empty:
+        return None
+    sents.append(non_empty[0])
+    for i in range(1,len(non_empty)):
+        s = non_empty[i]
+        if s == "`":
+            continue
+        if s in punct_set:
+            sents[-1] += s
+            continue
+        sents.append(s)
+    sents = [s.strip() for s in sents if len(s) > 3 or "bad" in s.lower()];
+    #sents = [s.strip() for s in sents if len(s)];
+    return sents
+
+def cbdq_json_dict(sents):
+    return {"origin" : sents}
 
 def main():
     parser = get_arg_parser()
@@ -46,15 +92,41 @@ def main():
 
     # Open File
     LOGGER.info("Opening file at %s", args.filename)
-    doc_text = None
+    raw_doc = None
     try:
         with open(args.filename, 'r') as file:
             LOGGER.info("Opened %s" % args.filename)
-            doc_text = file.read()
+            raw_doc = file.read()
     except IOError as e:
         LOGGER.error("Failed to open %s: %s" % (args.filename, e))
         return e.errno
 
+    # Format Doc
+    LOGGER.info("Formatting Document");
+    sents = get_sents(raw_doc)
+    num_too_long = 0
+    for i,s in enumerate(sents):
+        print("SENTENCE %d: '%s'" % (i, s))
+        threshold = 6
+        if len(s) == threshold:
+            print("WARNING: Exactly %d characters (%d): %s" % (len(s), len(s), s))
+            num_too_long += 1
+        #if len(s) > 280:
+        #    LOGGER.warning("Too many characters (%d): %s" % (len(s), s))
+        #    num_too_long += 1
+    LOGGER.info("Sentences: %d" % len(sents))
+    LOGGER.info("Sentences too long: %s" % num_too_long)
+
+    # Format sents for CPDQ
+    LOGGER.info("Converting to Cheap Bots Done Quick JSON format")
+    cbdq_dict = cbdq_json_dict(sents)
+    LOGGER.info("Saving JSON to %s" % args.output)
+    try:
+        with open(args.output, 'w') as file:
+            json.dump(cbdq_dict, file, indent=4)
+    except IOError as e:
+        LOGGER.error("Failed to save to %s: %s" % (args.output, e))
+        return e.errno
     return 0
 
 if __name__ == "__main__":
