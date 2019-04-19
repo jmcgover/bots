7bf1fab0e9cf9ae430ed918e1660b5a2c8e2d63d
Jeffrey D. McGovern <jeffreydmcgovern@gmail.com>
2019-04-18 23:31:26 -0700

    add arg parser to script

--- muellerreportbot/parse.py
+++ muellerreportbot/parse.py
@@ -3,7 +3,49 @@
 import os
 import sys
 
+import argparse
+
+# Logging
+import logging
+from logging import handlers
+LOGGER = logging.getLogger(__name__)
+SH = logging.StreamHandler()
+FH = logging.handlers.RotatingFileHandler("log.log", maxBytes=5 * 1000000, backupCount = 5)
+SH.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s:%(message)s"))
+FH.setFormatter(logging.Formatter("%(asctime)s:%(lineno)s:%(funcName)s:%(levelname)s:%(message)s"))
+LOGGER.setLevel(logging.DEBUG)
+LOGGER.addHandler(SH)
+LOGGER.addHandler(FH)
+
+DEFAULT_FILENAME="MuellerReport.txt"
+DESCRIPTION = """Parses the MuellerReport file into the appropriate data structures to enable text generation."""
+def get_arg_parser():
+    parser = argparse.ArgumentParser(prog=sys.argv[0], description=DESCRIPTION)
+    parser.add_argument("-f", "--filename",
+            help = "file to parse (default is %s)" % DEFAULT_FILENAME)
+    parser.add_argument("-i", "--info",
+            help = "set console logging output to INFO")
+    parser.add_argument("-d", "--debug",
+            help = "set console logging output to DEBUG")
+    parser.set_defaults(
+            filename = DEFAULT_FILENAME
+            )
+    return parser
+
+
 def main():
+    parser = get_arg_parser()
+    args = parser.parse_args()
+
+    # Logging Information
+    if args.info:
+        SH.setLevel(logging.INFO)
+    if args.debug:
+        SH.setLevel(logging.DEBUG)
+
+    # Open File
+    LOGGER.info("Opening file at %s", args.filename)
+
     return 0
 
 if __name__ == "__main__":
